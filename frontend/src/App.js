217dd31f8d25c757e2c0c7d73a812e781a178479
Pallav Agarwal <agpallav@gmail.com>
2019-11-11 04:08:46 -0500

    First page frontend completion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,89 @@
 import React, { Component } from "react";
+import { Navbar, Nav } from "react-bootstrap";
 import "./App.css";
-import Counters from "./components/counters";
 
 class App extends Component {
+  state = {
+    price: "$8,782",
+    priceInput: ""
+  };
+
+  handlePriceChange = e => {
+    this.setState({ priceInput: e.target.value });
+  };
+
+  placeBuy = () => {
+    let thePrice = this.state.priceInput;
+    if (!isNaN(thePrice) && thePrice.trim.length !== 0) {
+      alert("Successfully placed order of $" + thePrice + " BTC!");
+    } else {
+      alert("Please enter a number!");
+    }
+  };
+  placeSell = () => {};
+
+  navbar = () => {
+    return (
+      <Navbar sticky="top" className="theNav" variant="dark" expand="lg">
+        <Navbar.Brand className="navbar-brand mb-0 h1" href="#home">
+          Crypto Watch
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto"></Nav>
+          <Nav>
+            <Nav.Link className="tempWhite" href="#home">
+              Home
+            </Nav.Link>
+            <Nav.Link href="#orders">Orders</Nav.Link>
+            <Nav.Link href="#login">Login</Nav.Link>
+            <Nav.Link href="#signup">Sign Up</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  };
+
+  formInput = () => {
+    return (
+      <form>
+        <input
+          className="form-control form-control-lg btcEnter"
+          placeholder="Enter BTC Price $"
+          type="text"
+          name="order"
+          value={this.state.priceInput}
+          onChange={this.handlePriceChange}
+        />
+        <input
+          onClick={this.placeBuy}
+          className="btn btn-primary placeOrder btn-lg"
+          type="submit"
+          value="Store Buy Order"
+        />
+        &nbsp;&nbsp;
+        <input
+          onClick={this.placeBuy}
+          className="btn btn-primary placeOrder btn-lg"
+          type="submit"
+          value="Store Sell Order"
+        />
+      </form>
+    );
+  };
+
   render() {
     return (
-      <div>
-        <main className="container">
-          <Counters />
-        </main>
+      <div className="App">
+        {this.navbar()}
+        <header className="header">
+          <h1 className="">Crypto Watch</h1>
+          <img src={"https://bit.ly/2NAyCIX"} className="HeaderImg" alt="Header" />
+        </header>
 
-        <div className="App">
-          <a className="login" href="https://reactjs.org">
-            Sign Up
-          </a>
-          <a className="login" href="https://reactjs.org">
-            Login
-          </a>
-
-          <header className="App-header">
-            <h1>Portfolio Watch</h1>
-            <img
-              src={
-                "https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/05/04/15/bitcoin-price-latest-news-update.jpg?width=1000&height=614&fit=bounds&format=pjpg&auto=webp&quality=70&crop=16:9,offset-y0.5logo"
-              }
-              className="HeaderImg"
-              alt="Header"
-            />
-          </header>
+        <div className="priceView">
+          <h1>Current BTC Price: {this.state.price}</h1>
+          {this.formInput()}
         </div>
       </div>
     );
